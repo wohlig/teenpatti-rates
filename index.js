6c206b275b75875b121d0d2e96e65554935a6987
Chintan Shah <chintan@wohlig.com>
2020-03-24 19:13:06 +0530

    changes

--- index.js
+++ index.js
@@ -7,13 +7,17 @@ var Combinatorics = require("js-combinatorics");
 var allCards = _.map(Cards.getAllCards(), n => {
   return n.shortName;
 });
-var player1Cards = ["6s", "Jh", "Kc"];
-// var player1CardsRemaining = 3 - player1Cards.length;
-var player2Cards = ["4d", "8c"];
-// var player2CardsRemaining = 3 - player2Cards.length;
+// allCards = _.filter(allCards, function(n) {
+//   return /s/i.test(n);
+// });
+// console.log(allCards);
+var player1Cards = ["As"];
+var player1CardsRemaining = 3 - player1Cards.length;
+var player2Cards = [];
+var player2CardsRemaining = 3 - player2Cards.length;
 var usedCards = _.union(player1Cards, player2Cards);
 var remainingCards = _.xor(allCards, usedCards);
-var allCombinations = Combinatorics.permutation(
+var allCombinations = Combinatorics.bigCombination(
   remainingCards,
   6 - usedCards.length
 );
@@ -22,31 +26,31 @@ console.log(totalCombinations);
 var wins = { player1: 0, player2: 0, draw: 0 };
 var i = 0;
 while ((a = allCombinations.next())) {
-  // find each combination for 2 card and 3 cards
+  var tempPlayer1Cards = Combinatorics.combination(
+    a,
+    player1CardsRemaining
+  ).toArray();
+  console.log(tempPlayer1Cards);
 
-  // var tempPlayer1Cards = Combinatorics.combination(a, player1CardsRemaining);
-  // var tempFirstPlayerCards = Combinatorics.combination(
-  //   a,
-  //   player2CardsRemaining
-  // );
+  var bruteCards = _.map(tempPlayer1Cards, n => {
+    return {
+      player1Cards: _.union(player1Cards, n),
+      player2Cards: _.union(player2Cards, _.difference(a, n))
+    };
+  });
 
-  var newPlayer1Cards = _.cloneDeep(player1Cards);
-  var newPlayer2Cards = _.cloneDeep(player2Cards);
-  _.times(3 - newPlayer1Cards.length, () => {
-    newPlayer1Cards.push(a.shift());
+  _.each(bruteCards, cardsObj => {
+    var player1Score = teenPattiScore.scoreHandsNormal(cardsObj.player1Cards);
+    var player2Score = teenPattiScore.scoreHandsNormal(cardsObj.player2Cards);
+    if (player1Score.score > player2Score.score) {
+      wins.player1++;
+    } else if (player1Score.score < player2Score.score) {
+      wins.player2++;
+    } else {
+      wins.draw++;
+    }
   });
-  _.times(3 - newPlayer2Cards.length, () => {
-    newPlayer2Cards.push(a.shift());
-  });
-  var player1Score = teenPattiScore.scoreHandsNormal(newPlayer1Cards);
-  var player2Score = teenPattiScore.scoreHandsNormal(newPlayer2Cards);
-  if (player1Score.score > player2Score.score) {
-    wins.player1++;
-  } else if (player1Score.score < player2Score.score) {
-    wins.player2++;
-  } else {
-    wins.draw++;
-  }
+  return false;
 }
 
 wins.player1Probability = wins.player1 / totalCombinations;
